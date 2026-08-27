4bfc149ba58b3e091644a54b8375839fa7ed31a3
Ronic Vasane <ronicvasane@gmail.com>
2026-08-27 17:18:49 +0530

    inital commit - digit classfier API

---
+++ ronic_app.py
@@ -0,0 +1,41 @@
+from flask import Flask, request, jsonify
+import tensorflow as tf
+import numpy as np
+from PIL import Image
+import io
+
+app = Flask(__name__)
+
+
+model = tf.keras.models.load_model('digit_classifier.h5')
+
+@app.route('/')
+def home():
+    return "Digit Classifier API is running!"
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    
+    file = request.files['image']
+    img = Image.open(io.BytesIO(file.read())).convert('L')  
+    img = img.resize((28, 28))  
+
+    
+    img_array = np.array(img) / 255.0
+    img_array = img_array.reshape(1, 28, 28)
+
+    
+    prediction = model.predict(img_array)
+    predicted_digit = int(np.argmax(prediction))
+    confidence = float(np.max(prediction))
+
+    return jsonify({
+        'predicted_digit': predicted_digit,
+        'confidence': round(confidence, 3)
+    })
+
+import os
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
