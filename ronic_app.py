f89e4364a0cd8509ed0f73276b6a9e4369c9cbf4
ronicvasane <ronicvasane@gmail.com>
2026-08-29 00:04:54 +0530

    Add HTML rendering and clean up predict function

--- ronic_app.py
+++ ronic_app.py
@@ -1,30 +1,27 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 import tensorflow as tf
 import numpy as np
 from PIL import Image
 import io
+import os
 
 app = Flask(__name__)
-
 
 model = tf.keras.models.load_model('digit_classifier.h5')
 
 @app.route('/')
 def home():
-    return "Digit Classifier API is running!"
+    return render_template('index.html')
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    
     file = request.files['image']
-    img = Image.open(io.BytesIO(file.read())).convert('L')  
-    img = img.resize((28, 28))  
+    img = Image.open(io.BytesIO(file.read())).convert('L')
+    img = img.resize((28, 28))
 
-    
     img_array = np.array(img) / 255.0
     img_array = img_array.reshape(1, 28, 28)
 
-    
     prediction = model.predict(img_array)
     predicted_digit = int(np.argmax(prediction))
     confidence = float(np.max(prediction))
@@ -33,8 +30,6 @@ def predict():
         'predicted_digit': predicted_digit,
         'confidence': round(confidence, 3)
     })
-
-import os
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
